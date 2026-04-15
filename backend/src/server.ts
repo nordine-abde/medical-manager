a2532ec2340e2f6224a5d90757ba93e6aff94e80
a.nordine <abdessamad.nordine.github@gmail.com>
2026-04-15 14:17:32 +0200

    logging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 import { appConfig } from "./config/env";
+import { logger } from "./logging";
 
 app.listen({
   hostname: appConfig.host,
@@ -9,3 +10,7 @@ app.listen({
 console.log(
   `${appConfig.appName} listening on http://${appConfig.host}:${appConfig.port}${appConfig.apiPrefix}`,
 );
+
+logger.info("log_level_active", {
+  logLevel: appConfig.logLevel,
+});
